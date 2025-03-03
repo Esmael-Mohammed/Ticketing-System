2421ff5218c38e9c9bb55ecceaa2b599ae3fd526
Esmael-Mohammed <esmaelmohammed01@gmail.com>
2025-03-03 09:09:43 +0300

    initialize node module

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+require('dotenv').config();
+
+const app=express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
